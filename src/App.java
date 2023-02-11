26404253b37a43750e7e4190ce0fc2082d1b0fef
Kamsi <ozorjikj@gmail.com>
2023-02-11 15:36:23 -0600

    Create greeting and ask first question

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Welcome to the Department of Motor Vehicles Virtual Assistant also known as DMVA");
+        System.out.println("How can I assist you today?");
     }
 }
