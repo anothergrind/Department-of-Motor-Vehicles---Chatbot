e32a603a002e68f3a2c7c4a70a4e181ea6fdf63d
Kamsi <ozorjikj@gmail.com>
2023-02-12 18:49:13 -0600

    Added options class

--- src/App.java
+++ src/App.java
@@ -3,35 +3,76 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         
-        Scanner input = new Scanner(System.in);
+        try (Scanner input = new Scanner(System.in)) {
+            // Brief greeting 
+            System.out.println("Welcome to the Department of Motor Vehicles Virtual Assistant also known as DMVA");
+            
+            //Starter question
+            System.out.println("How can I assist you today?");
+            
+            // List that stores the user's options 
+            ArrayList<Integer> user_options = new ArrayList<Integer>();
+            for(int i = 1; i <= 10; i++)
+            {
+                user_options.add(i);
+            }
 
-        // Brief greeting 
-        System.out.println("Welcome to the Department of Motor Vehicles Virtual Assistant also known as DMVA");
-        
-        //Starter question
-        System.out.println("How can I assist you today?");
-        
-        // List that stores the user's options 
-        ArrayList<Integer> user_options = new ArrayList<Integer>();
-        for(int i = 1; i <= 10; i++)
-        {
-            user_options.add(i);
+            // List of options that the user has
+            System.out.println("Press 1 to look up your car");
+            System.out.println("Press 2 to renew your car");
+            System.out.println("Press 3 to sign up for license test");
+            System.out.println("Press 4 to pay for fees");
+            System.out.println("Press 5 to change the address on your driver's license");
+            System.out.println("Press 6 for information on how to obtain a driver's license");
+            System.out.println("Press 7 for information on how to obtain a motorcyle license");
+            System.out.println("Press 8 for information on obtaining a commerical driver's license ");
+            System.out.println("Press 9 to report a lost or stolen driver's license");
+            System.out.println("Press 10 for general information and FAQs");
+
+            // Allowing the user to make a decision
+            System.out.print("Please make a decision: ");
+            int user_decision = input.nextInt();
+
+            // Creating an object 
+            Options options = new Options();
+
+            // Calling an options method based on user input 
+            switch(user_decision){
+                case 1:
+                    options.look_up_car();
+                    break;
+                case 2: 
+                    options.renew_car();
+                    break;
+                case 3:
+                    options.sign_up();
+                    break;
+                case 4: 
+                    options.pay();
+                    break;
+                case 5: 
+                    options.change_address();
+                    break;
+                case 6: 
+                    options.get_drivers_license();
+                    break;
+                case 7: 
+                    options.get_motorcycle_license();
+                    break;
+                case 8: 
+                    options.get_commercial_license();
+                    break;
+                case 9: 
+                    options.report_license();
+                    break;
+                case 10:
+                    options.get_general_info();
+                    break;
+
+                default: 
+                    System.out.println("The option that you selected does not exist. Please try again");
+
+            }
         }
-
-        // List of options that the user has
-        System.out.println("Press 1 to look up your car");
-        System.out.println("Press 2 to renew your car");
-        System.out.println("Press 3 to sign up for license test");
-        System.out.println("Press 4 to pay for fees");
-        System.out.println("Press 5 to change the address on your driver's license");
-        System.out.println("Press 6 for information on how to obtain a driver's license");
-        System.out.println("Press 7 for information on how to obtain a motorcyle license");
-        System.out.println("Press 8 for information on obtaining a commerical driver's license ");
-        System.out.println("Press 9 to report a lost or stolen driver's license");
-        System.out.println("Press 10 for general information and FAQs");
-
-        // Allowing the user to make a decision
-        System.out.print("Please make a decision: ");
-        int user_decision = input.nextInt();
     }
 }
---
+++ src/Options.java
@@ -0,0 +1,45 @@
+/*
+ * This class is supposed to hold all of the potential options a user has
+ * in the chatbot
+ */
+public class Options {
+    public void look_up_car(){
+
+    }
+
+    public void renew_car(){
+
+    }
+
+    public void sign_up(){
+
+    }
+
+    public void pay(){
+
+    }
+
+    public void change_address(){
+
+    }
+
+    public void get_drivers_license(){
+
+    }
+    public void get_motorcycle_license(){
+
+    }
+
+    public void get_commercial_license(){
+
+    }
+
+    public void report_license(){
+
+    }
+
+    public void get_general_info(){
+
+    }
+
+}
